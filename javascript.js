10c74f5b2fea4aca33fcf3e1583dc14ebc0954ad
Sara Parizi <saraprz.dev@outlook.com>
2025-02-02 17:11:39 -0300

    Aula 4 Finish

--- javascript.js
+++ javascript.js
@@ -2,16 +2,16 @@ const searchInput = document.getElementById('search-input');
 const resultArtist = document.getElementById("result-artist");
 const resultPlaylist = document.getElementById('result-playlists');
 
-
 function requestApi(searchTerm) {
-    const url = `http://localhost:3000/artists?name_like=${searchTerm}`
+    const url = `http://localhost:3000/artists?name_like=${searchTerm}`;
     fetch(url)
         .then((response) => response.json())
         .then((result) => displayResults(result))
+        .catch((error) => console.error('Error fetching data:', error)); 
 }
 
 function displayResults(result) {
-    resultPlaylist.classList.add("hidden")
+    resultPlaylist.classList.add("hidden");
     const artistName = document.getElementById('artist-name');
     const artistImage = document.getElementById('artist-img');
 
@@ -23,15 +23,12 @@ function displayResults(result) {
     resultArtist.classList.remove('hidden');
 }
 
-
 document.addEventListener('input', function () {
     const searchTerm = searchInput.value.toLowerCase();
     if (searchTerm === '') {
-        resultPlaylist.classList.add('hidden');
-        resultArtist.classList.remove('hidden');
-        return
+        resultArtist.classList.add('hidden');
+        resultPlaylist.classList.remove('hidden');
+    } else {
+        requestApi(searchTerm);
     }
-    
-    requestApi(searchTerm);
-})
-
+});
